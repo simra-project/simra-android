943b97a1d3409375e0cc1c19f8fb031e603eca60
m.bauknecht <m.bauknecht@campus.tu-berlin.de>
2018-11-17 16:22:14 +0100

    burger finish
    navigation drwaer works, tha bar is a bit smaler than in earlier versions

--- app/src/main/java/app/com/example/android/octeight/MainActivity.java
+++ app/src/main/java/app/com/example/android/octeight/MainActivity.java
@@ -10,11 +10,16 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -58,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // CLICKABLES --> INTENTS
 
-    ImageButton menuButton;
+    //ImageButton menuButton;
     ImageButton helmetButton;
 
     RelativeLayout neuRoute;
@@ -178,20 +183,16 @@ public class MainActivity extends AppCompatActivity {
         // CLICKABLES
 
         // (1): Burger Menu
-
-        menuButton = findViewById(R.id.burger_menu);
-         menuButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent launchActivityIntent = new Intent(MainActivity.this,
-                        MenuActivity.class);
-                startActivity(launchActivityIntent);
-            }
-        });
-
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //menuButton = findViewById(R.id.burger_menu);
+        //menuButton.setOnClickListener(new View.OnClickListener() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(MainActivity.this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                drawer.addDrawerListener(toggle);
+                toggle.syncState();
         // (2): Helmet
 
-        helmetButton = findViewById(R.id.helmet_icon);
+       /* helmetButton = findViewById(R.id.helmet_icon);
         helmetButton.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -199,8 +200,24 @@ public class MainActivity extends AppCompatActivity {
         HelmetActivity.class);
         startActivity(launchActivityIntent);
         }
-        });
+        });*/
 
+       // (2.1): Toolbar
+
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        // navigationView.bringToFront();
+        // navigationView.setNavigationItemSelectedListener(this);
+*/
 
         // (3): Neue Route
 
@@ -468,5 +485,81 @@ public class MainActivity extends AppCompatActivity {
         }
 
     };
+
+    // Navigation Drawer
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+   /* @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    // rechtes Toolbar icon
+   /* public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_history) {
+            // Handle the camera action
+        } else if (id == R.id.nav_democraphic_data) {
+            // src: https://stackoverflow.com/questions/2197741/how-can-i-send-emails-from-my-android-application
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("message/rfc822");
+            i.putExtra(Intent.EXTRA_EMAIL  , new String[]{getString(R.string.feedbackReceiver)});
+            i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.demoDataHeader));
+            i.putExtra(Intent.EXTRA_TEXT, getString(R.string.demoDataMail));
+            try {
+                startActivity(Intent.createChooser(i, "Send Data..."));
+            } catch (android.content.ActivityNotFoundException ex) {
+                Toast.makeText(MainActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+            }
+
+        } else if (id == R.id.nav_feedback) {
+            // src: https://stackoverflow.com/questions/2197741/how-can-i-send-emails-from-my-android-application
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("message/rfc822");
+            i.putExtra(Intent.EXTRA_EMAIL  , new String[]{getString(R.string.feedbackReceiver)});
+            i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.feedbackHeader));
+            i.putExtra(Intent.EXTRA_TEXT, getString(R.string.feedbackMail));
+            try {
+                startActivity(Intent.createChooser(i, "Send mail..."));
+            } catch (android.content.ActivityNotFoundException ex) {
+                Toast.makeText(MainActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+            }
+
+        } else if (id == R.id.nav_setting) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
 }
