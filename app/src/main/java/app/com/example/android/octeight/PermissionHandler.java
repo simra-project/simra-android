3586f6181eb707bd6b4e8052cc12ef4e24b6c3a5
selphiron <ahmetserdar@hotmail.de>
2018-11-28 08:11:04 +0100

    gps permission request + message
    https://trello.com/c/5p18sc2T
    Feature: Create a message that is displayed before the gps permission request

--- app/src/main/java/app/com/example/android/octeight/PermissionHandler.java
+++ app/src/main/java/app/com/example/android/octeight/PermissionHandler.java
@@ -35,11 +35,38 @@ public class PermissionHandler {
 
     }
 
+
     public static synchronized void askPermission(Activity activity) {
 
-        activity.requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
-                LOCATION_ACCESS_CODE);
+        final Activity helper = activity;
 
+        if (!shouldShowRequestPermissionRationale(helper, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            showMessageOKCancel("Um fortzufahren, erlaube im folgenden Fenster bitte den Zugriff auf" +
+                            " Deine Standortdaten.",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            helper.requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                                    LOCATION_ACCESS_CODE);
+                        }
+                    }, helper);
+            return;
+        }
+
+        helper.requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                LOCATION_ACCESS_CODE);
+        return;
+
+    }
+
+    private static synchronized void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener,
+                                                         Activity activity) {
+        new android.support.v7.app.AlertDialog.Builder(activity)
+                .setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton("OK", okListener)
+                .create()
+                .show();
     }
 
 
