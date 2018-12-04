9b89c561039eca6cb23e1c1e1b9891061de384e5
selphiron <ahmetserdar@hotmail.de>
2018-12-04 15:22:46 +0100

    fixed StartActivity
    - Now StartActivity is not displayed when the android back button is pressed in the MainActivity
    - MainActivity is not started a second time, when the okay button is pressed in StartActivity
    - Added comments
    - Removed unused variables

--- app/src/main/java/app/com/example/android/octeight/StartActivity.java
+++ app/src/main/java/app/com/example/android/octeight/StartActivity.java
@@ -7,36 +7,54 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+/**
+ * Shows general info about the app and starts the MainActivity once the okay-Button is pressed
+ * or TIME_OUT/1000 seconds are passed.
+ * TODO: migrate permission request and map loading from MainActivity to StartActivity
+ */
+
 public class StartActivity extends AppCompatActivity {
 
     private static int TIME_OUT = 10000; //Time to launch the another activity
     Button next;
+    Runnable startActivityRunnable;
+    Handler startActivityHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        // start MainActivity after clicking the Button
-        next = findViewById(R.id.nextBtn);
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intend = new Intent (StartActivity.this, MainActivity.class);
-                startActivity(intend);
-            }
-        });
-
-        // start MainActivity after defined time
-        final View myLayout = findViewById(R.id.startScreen);
-        new Handler().postDelayed(new Runnable() {
+        // Runnable that starts MainActivity after defined time (TIME_OUT)
+        startActivityRunnable = new Runnable() {
             @Override
             public void run() {
                 Intent i = new Intent(StartActivity.this, MainActivity.class);
                 startActivity(i);
+                // finish() to prevent going back to StartActivity, when the Back Button is pressed
+                // in MainActivity
                 finish();
             }
-        }, TIME_OUT);
+        };
+        // create Handler and make it run the Runnable after TIME_OUT
+        startActivityHandler = new Handler();
+        startActivityHandler.postDelayed(startActivityRunnable, TIME_OUT);
+
+        // start MainActivity when Button is clicked
+        next = findViewById(R.id.nextBtn);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (StartActivity.this, MainActivity.class);
+                // remove the Callback of the Runnable to the Handler to prevent second start of
+                // MainActivity
+                startActivityHandler.removeCallbacks(startActivityRunnable);
+                startActivity(intent);
+                // finish() to prevent going back to StartActivity, when the Back Button is pressed
+                // in MainActivity
+                finish();
+            }
+        });
     }
 }
 
