3087ee22481bece2a040f6336e161b4467a71319
selphiron <ahmetserdar@hotmail.de>
2019-01-08 15:24:58 +0100

    notification (wip)

--- app/src/main/java/app/com/example/android/octeight/RecorderService.java
+++ app/src/main/java/app/com/example/android/octeight/RecorderService.java
@@ -2,6 +2,9 @@ package app.com.example.android.octeight;
 
 import android.annotation.SuppressLint;
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -12,9 +15,12 @@ import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Process;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import java.io.File;
@@ -47,6 +53,10 @@ public class RecorderService extends Service implements SensorEventListener, Loc
     private File gpsFile;
     LocationManager locationManager;
     Location lastLocation;
+    int notificationId = 1337;
+    NotificationManagerCompat notificationManager;
+
+
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // SensorEventListener Methods
@@ -147,6 +157,13 @@ public class RecorderService extends Service implements SensorEventListener, Loc
         // Executor service for writing data
         executor = Executors.newSingleThreadExecutor();
 
+        // Notification
+        NotificationCompat.Builder mBuilder = createNotification();
+        notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(notificationId, mBuilder.build());
+
     }
 
     @SuppressLint("MissingPermission")
@@ -193,6 +210,9 @@ public class RecorderService extends Service implements SensorEventListener, Loc
 
             }
         }).start();
+
+        // Remove the Notification
+        notificationManager.cancel(notificationId);
 
     }
 
@@ -276,12 +296,39 @@ public class RecorderService extends Service implements SensorEventListener, Loc
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            }
         }
+    }
 
-
-
+    private NotificationCompat.Builder createNotification() {
+        String CHANNEL_ID = "RecorderServiceNotification";
+        /*
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        */
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_nameDE);
+            String description = getString(R.string.channel_descriptionDE);
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
         }
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.helmet)
+                .setContentTitle("Aufzeichnung der Fahrt")
+                .setContentText("Ihre Fahrt wird aufgezeichnet.")
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                // Set the intent that will fire when the user taps the notification
+                /*.setContentIntent(pendingIntent)*/;
 
+        return mBuilder;
 
     }
 
