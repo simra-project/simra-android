efcc80136b65b1dfed718839bda16704c1f24460
m.bauknecht <m.bauknecht@campus.tu-berlin.de>
2019-03-01 13:46:31 +0100

    Credits finished

---
+++ app/src/main/java/app/com/example/android/octeight/CreditsActivity.java
@@ -0,0 +1,21 @@
+package app.com.example.android.octeight;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class CreditsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
