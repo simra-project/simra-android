e14cfb5f1f976577196502e32482afb272a46259
mbauknecht <43986726+mbauknecht@users.noreply.github.com>
2019-03-04 13:42:23 +0100

    Bug fixed & ActionBar simplified

--- app/src/main/java/app/com/example/android/octeight/CreditsActivity.java
+++ app/src/main/java/app/com/example/android/octeight/CreditsActivity.java
@@ -13,7 +13,7 @@ public class CreditsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
     }
--- app/src/main/java/app/com/example/android/octeight/Impressum2Activity.java
+++ app/src/main/java/app/com/example/android/octeight/Impressum2Activity.java
@@ -1,5 +1,6 @@
 package app.com.example.android.octeight;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,8 +21,9 @@ public class Impressum2Activity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(Impressum2Activity.this, WebActivity.class);
+                intent.putExtra("URL", getString(R.string.dsbContact));
+                startActivity(intent);
             }
         });
     }
