d3492d1ac01d87a65c37db08c393d2574d1b7401
selphiron <ahmetserdar@hotmail.de>
2019-03-22 10:01:20 +0100

    de.tuberlin.mcc.simra.app

---
+++ app/src/main/java/de/tuberlin/mcc/simra/app/BaseActivity.java
@@ -0,0 +1,14 @@
+package de.tuberlin.mcc.simra.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new LoggingExceptionActivity(BaseActivity.this);
+    }
+
+}
