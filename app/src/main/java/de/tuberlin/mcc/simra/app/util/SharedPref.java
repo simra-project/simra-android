19d129ba04ddd5509fbb569e6bef927cb18fc1c6
Daniel Habenicht <daniel-habenicht@outlook.de>
2020-06-12 16:09:51 +0000

    Updated SharedPref.java to use application context

--- app/src/main/java/de/tuberlin/mcc/simra/app/util/SharedPref.java
+++ app/src/main/java/de/tuberlin/mcc/simra/app/util/SharedPref.java
@@ -9,10 +9,10 @@ public class SharedPref {
         public static class Radmesser {
             public static final String RADMESSER_ENABLED = "RADMESSER_ENABLED";
             public static boolean isEnabled(Context ctx){
-                return ctx.getSharedPreferences(SHARED_PREF_NAME, DEFAULT_MODE).getBoolean(RADMESSER_ENABLED, false);
+                return ctx.getApplicationContext().getSharedPreferences(SHARED_PREF_NAME, DEFAULT_MODE).getBoolean(RADMESSER_ENABLED, false);
             }
             public static void setEnabled(Boolean enabled, Context ctx){
-                ctx.getSharedPreferences(SHARED_PREF_NAME, DEFAULT_MODE).edit().putBoolean(RADMESSER_ENABLED, enabled).apply();
+                ctx.getApplicationContext().getSharedPreferences(SHARED_PREF_NAME, DEFAULT_MODE).edit().putBoolean(RADMESSER_ENABLED, enabled).apply();
             }
         }
 
